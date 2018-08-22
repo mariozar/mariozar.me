ffaa12456b3c028ab1e53e125c349bfc586bd4c9
krazer <charzar@MacBook-Pro-3.local>
2018-08-21 19:40:01 -0700

    updates

--- my-app/src/components/Resume.js
+++ my-app/src/components/Resume.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import { Document, Page } from 'react-pdf';
 
+
+
 class Resume extends Component {
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  }
+
+  onDocumentLoad = ({ numPages }) => {
+    this.setState({ numPages });
+  }
+
   render() {
+    const { pageNumber, numPages } = this.state;
+
     return (
       <div>
         <Document
-          file="./arcgis.pdf"
+          file="test.pdf"
           onLoadSuccess={this.onDocumentLoad}
         >
+          <Page pageNumber={pageNumber} />
         </Document>
+        <p>Page {pageNumber} of {numPages}</p>
       </div>
     );
   }
