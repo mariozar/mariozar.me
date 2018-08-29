4e3f4c198c71e6f6681288e8f2b09bc240f4c34f
krazer <charzar@macbook-pro-3.home>
2018-08-29 02:35:47 -0700

    customized color scheme

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
-import Resume from './components/Resume';
+//import Resume from './components/Resume';
 import Terminal from 'terminal-in-react';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Resume />
+      <div >
+        <NavBar />
+        <Terminal
+          color='white'
+          backgroundColor='#515151'
+          backgroundColor='#333333'
+          barColor='black'
+          prompt='white'
+          hideTopBar='true'
+          startState='maximised'
+          style={{ fontWeight: "bold", fontSize: "1em", height: "100vh"}}
+          commands={{
+            'open-google': () => window.open('https://www.google.com/', '_blank'),
+            showmsg: this.showMsg,
+            popup: () => alert('Terminal in React')
+          }}
+          descriptions={{
+            'open-google': 'opens google.com',
+            showmsg: 'shows a message',
+            alert: 'alert', popup: 'alert'
+          }}
+          msg='Welcome!'
+        />
       </div>
     );
   }
--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
+import React from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const defaultTheme = createMuiTheme();
 
 const NavBar = () => {
     return(
         <div>
-        <AppBar position="static">
+        <AppBar position="static" >
             <Toolbar>
                 <Typography variant="title" color="inherit">
-                React & Material-UI Sample Application
+                Mario Salazar
                 </Typography>
             </Toolbar>
         </AppBar>
